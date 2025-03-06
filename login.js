29d639b18bc82059556f7ae5b7c1dc9beb035c11
jenniferbogado1 <127226927+jenniferbogado1@users.noreply.github.com>
2025-03-06 20:05:23 -0300

    Actualizar login.js

--- login.js
+++ login.js
@@ -63,3 +63,33 @@ async function login() {
 if (localStorage.getItem("loggedUser")) {
     window.location.href = "menu.html"; // Si ya está logueado, redirige automáticamente
 }
+
+
+
+//codigo antes 
+
+// Usuarios predefinidos
+const users = {
+    "user": { password: "1234", role: "user" },
+    "admin": { password: "admin", role: "admin" }
+};
+
+// Función para iniciar sesión
+function login() {
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const errorMsg = document.getElementById("login-error");
+
+    if (users[username] && users[username].password === password) {
+        // Guardar usuario en sesión
+        localStorage.setItem("loggedUser", username);
+        window.location.href = "menu.html"; // Redirigir a la página de películas
+    } else {
+        errorMsg.style.display = "block";
+    }
+}
+
+// Verificar si ya hay sesión activa
+if (localStorage.getItem("loggedUser")) {
+    window.location.href = "menu.html"; // Si ya está logueado, redirige automáticamente
+}
